bc4aa2dfdeb74f857dfc39c5a51c97cf9b4b580c
Constverum <idenizk@gmail.com>
2015-10-25 19:57:39 +0300

    a few edits to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages, find_package_data
+from setuptools import setup, find_packages #  find_package_data
 
 PACKAGE = "pyproxychecker"
 NAME = PACKAGE
@@ -8,23 +8,27 @@ AUTHOR_EMAIL = "constverum@gmail.com"
 URL = "https://github.com/constverum/PyProxyChecker"
 VERSION = __import__(PACKAGE).__version__
 
+with open("README.rst", 'r') as f:
+    longDescr = f.read()
+
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.rst"),
+    long_description=longDescr,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="Apache License, Version 2.0",
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     install_requires=[],
-    platforms = "any",
-    package_data=find_package_data(
-            PACKAGE,
-            only_in_packages=False),
+    platforms="any",
+    # package_data=[PACKAGE],
+    # package_data=find_package_data(
+    #         PACKAGE,
+    #         only_in_packages=False),
     classifiers=[
-        "Development Status :: 3 - Alpha"
+        "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
