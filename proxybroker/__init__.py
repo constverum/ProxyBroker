5cdcbcca362e39386df9dc12bbbe14eb7ed4e66a
Constverum <constverum@gmail.com>
2016-03-08 14:28:57 +0300

    cleanup

--- proxybroker/__init__.py
+++ proxybroker/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright © 2015 Constverum <constverum@gmail.com>. All rights reserved.
+Copyright © 2015-2016 Constverum <constverum@gmail.com>. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,10 +15,15 @@ limitations under the License.
 """
 
 __title__ = 'ProxyBroker'
-__version__ = "0.1.2"
+__package__ = 'proxybroker'
+__version__ = '0.1.2'
+__short_description__ = '[Finder/Grabber/Checker] Finds public proxies on multiple sources and concurrently checks them (type, anonymity, country). HTTP(S) & SOCKS'
 __author__ = 'Constverum'
-__license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2015 Constverum'
+__author_email__ = 'constverum@gmail.com'
+__url__ = 'https://github.com/constverum/ProxyBroker'
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = 'Copyright 2015-2016 Constverum'
+
 
 from .proxy import Proxy
 from .judge import Judge
--- setup.py
+++ setup.py
@@ -1,58 +1,44 @@
+import re
+from setuptools import setup
+
+
 # https://packaging.python.org/en/latest/distributing/
-from setuptools import setup  # find_packages, find_package_data
 
-PACKAGE = "proxybroker"
-NAME = PACKAGE
-DESCRIPTION = "[Finder/Grabber/Checker] Finds public proxies on multiple sources and concurrently checks them (type, anonymity, country). Supports HTTP(S) & SOCKS"
-AUTHOR = "Constverum"
-AUTHOR_EMAIL = "constverum@gmail.com"
-URL = "https://github.com/constverum/ProxyBroker"
-VERSION = __import__(PACKAGE).__version__
+with open('proxybroker/__init__.py', 'r') as f:
+    INFO = dict(re.findall(r"__(\w+)__ = '([^']+)'", f.read(), re.MULTILINE))
 
-with open("README.rst", 'r') as f:
-    longDescr = f.read()
+with open('README.rst', 'r') as f:
+    INFO['long_description'] = f.read()
 
-packages = ['proxybroker',
-            'proxybroker.data']
-requires = ['aiodns', 'aiohttp', 'maxminddb']
+REQUIRES = ['aiodns', 'aiohttp', 'maxminddb']
+PACKAGES = ['proxybroker', 'proxybroker.data']
+PACKAGE_DATA = {'': ['LICENSE'], INFO['package']: ['data/*.gif', 'data/*.mmdb']}
 
 setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=longDescr,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    license="Apache License, Version 2.0",
-    url=URL,
-    packages=packages,
-    install_requires=requires,
-    package_data={NAME: ['LICENSE', 'data/*.txt', 'data/*.mmdb']},
-    platforms="any",
+    name=INFO['package'],
+    version=INFO['version'],
+    description=INFO['short_description'],
+    long_description=INFO['long_description'],
+    author=INFO['author'],
+    author_email=INFO['author_email'],
+    license=INFO['license'],
+    url=INFO['url'],
+    install_requires=REQUIRES,
+    packages=PACKAGES,
+    package_data=PACKAGE_DATA,
+    platforms='any',
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.5",
-        "Operating System :: OS Independent",
-        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
-        "Topic :: Internet :: Proxy Servers",
-        "License :: OSI Approved :: Apache Software License",
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Internet :: Proxy Servers',
+        'License :: OSI Approved :: Apache Software License',
     ],
     keywords='proxy finder grabber scrapper checker broker async asynchronous http https connect socks socks4 socks5',
     zip_safe=False,
     test_suite='tests',
 )
-
-# packages=find_packages(exclude=["tests.*", "tests"]),
-# package_data=[PACKAGE],
-# package_data=find_package_data(
-#         PACKAGE,
-#         only_in_packages=False),
-# include_package_data=True,
-# package_data = {
-#     NAME: ['data/*.txt', 'data/*.mmdb', 'data/*.dat', 'LICENSE'],},
-# data_files=[
-#     ('/opt/local/myproject/etc', ['myproject/config/settings.py', 'myproject/config/other_settings.special']),
-# ],
