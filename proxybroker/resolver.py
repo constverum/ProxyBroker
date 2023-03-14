0de979f4f38d4615d308ff3cacfa941b57d0509b
Stefan Machmeier <stefan-machmeier@outlook.com>
2023-03-14 17:38:29 +0100

    Fix leading zero issue in octet

--- proxybroker/resolver.py
+++ proxybroker/resolver.py
@@ -52,8 +52,9 @@ class Resolver:
         """Check a host is IP address."""
         # TODO: add IPv6 support
         try:
+            host = '.'.join(f'{int(n):03d}' for n in host.split('.'))
             ipaddress.IPv4Address(host)
-        except ipaddress.AddressValueError:
+        except (ipaddress.AddressValueError, builtins.ValueError):
             return False
         else:
             return True
