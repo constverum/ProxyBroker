7c326b60d2e138ae04196010e812aadb5fb441e0
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2023-03-25 03:52:03 +0800

    update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-VERSION=v2.0.0-alpha5
+VERSION=v2.0.0-alpha6
 
 docker build --pull -t bluet/proxybroker2 .
 docker scan bluet/proxybroker2:latest
 
 docker tag bluet/proxybroker2:latest bluet/proxybroker2:${VERSION}
-git tag "${VERSION}" -a -m "proxybroker2 ${VERSION}"
-git push
-git push --tags
+
+while true; do
+        read -p "Add new git tag v${VERSION} and push? (Have you git add and git commit already?) [y/N]" yn
+        case $yn in
+                [Yy]* ) git tag "v${VERSION}" -a -m "v${VERSION}" && git push && git push --tags; break;;
+                [Nn]* ) exit;;
+                * ) echo "";;
+        esac
+done
 
 # Fixes busybox trigger error https://github.com/tonistiigi/xx/issues/36#issuecomment-926876468
 docker run --privileged -it --rm tonistiigi/binfmt --install all
@@ -16,7 +22,7 @@ docker run --privileged -it --rm tonistiigi/binfmt --install all
 docker buildx create --use
 
 while true; do
-        read -p "Have I Updated VERSION Info? (Is current VERSION=${VERSION} ?) [y/N]" yn
+        read -p "Build for multi-platform and push? (Have I Updated VERSION Info? Is the latest VERSION=${VERSION} ?) [y/N]" yn
         case $yn in
                 [Yy]* ) docker buildx build -t bluet/proxybroker2:latest -t bluet/proxybroker2:${VERSION} --platform linux/amd64,linux/arm64/v8 --pull --push .; break;;
                 [Nn]* ) exit;;
